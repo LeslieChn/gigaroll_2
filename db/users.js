5acf23ed11980e1111a2b88bb973d7380ed7cdea
Preston Carey <pcarey1213@gmail.com>
2021-07-01 12:36:56 -0400

    balpreet user info

--- db/users.js
+++ db/users.js
@@ -2,7 +2,8 @@ var records = [
   // { id: 1, username: 'prestoncarey', password: 'password', displayName: 'Preston', emails: [ { value: 'pcarey1213@gmail.com' } ] },
   { id: 1, "username":"prestoncarey","password":"6ff2b2d724362e153b56519bcb7f28f8", "displayName":"Preston"},
   { id: 2, "username":"raminfarz", "password":"8959753740a2818ef1c23b9250c676d7", "displayName": "Ramin" },
-  { id: 3, "username":"lzhang","password":"3aeb9fe54b40f02090795bc1ecedb956", "displayName":"Leslie "}
+  { id: 3, "username":"lzhang","password":"3aeb9fe54b40f02090795bc1ecedb956", "displayName":"Leslie "},
+  { id: 4, "username":"bkaur","password":"280af113c9c2f28d5501958ce8c3d386", "displayName":"Balpreet"}
 ];
 
 exports.findById = function(id, cb) {
